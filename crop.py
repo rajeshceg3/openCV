a2faa7f633eaf441db3c58186460d182a9d2fdf9
Rajesh <rajeshceg3@gmail.com>
2020-02-18 11:04:51 +0530

    Update crop.py

--- crop.py
+++ crop.py
@@ -3,7 +3,7 @@ import cv2
 image = cv2.imread("faces.png")
 cv2.imshow("Without Crop", image)
 
-image[ybegin:yend, xbegin:xend]
+#Cropping :- image[ybegin:yend, xbegin:xend]
 new_img = image[60:160, 300:380]
 cv2.imshow("After Crop", new_img)
 cv2.waitKey(0)
